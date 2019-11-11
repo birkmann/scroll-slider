f0c86f8f1884ad8c709c0f2e07c4f027de51a81f
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-11-11 21:32:55 +0100

    files

--- js/scroll-slider.js
+++ js/scroll-slider.js
@@ -1,25 +1,34 @@
 (function ($) {
 
-    $.fn.scrollSlider = function (options) {
+	$.fn.scrollSlider = function (options) {
 
-        var settings = $.extend({
-            'autoplay': false
-        }, options);
+		var settings = $.extend({
+			'autoplay': false,
+			'fixedbg': true
+		}, options);
 
-        return this.each(function () {
-            var this_slider = this;
-            var slides;
+		return this.each(function () {
+			var this_slider = this;
+			var slides;
 			var count = $(this_slider).children('*:not(.nav)').length;
 
 			if (count > 0) {
 				slides = $(this_slider).children('*:not(.nav)').find(".slide");
 				
-				$(slides).each(function () {
+				$(slides).each(function (i) {
 					var bgImage = $(this).find(".bg").data('bg');
+					$(this).attr('id', 'scroll-slide-' + i);
 					$(this).find(".bg").css("background-image", "url(' " + bgImage + " ')");
 				});
+
+				$(this).append("<nav class=nav-right><ul></ul></nav>");
+
+				for (var i = 0; i <= slides.length - 1; i++) {
+					$(this).find(".nav-right ul").append("<li><a href=#scroll-slide-"+i+"><span></span></a></li>");
+				}
 			}
 
-        });
-    }
+		});
+
+	}
 })(jQuery);
